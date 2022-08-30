001e2deb11d7943163c1123ee13b1b975eb6c610
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-08-30 18:16:00 -0400

    Install packages and work on classes

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,5 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../lib/managers.js");
+const Employee = require("../lib/employees.js");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
---
+++ index.js
@@ -0,0 +1,4 @@
+const Profile = require("./lib/employees.js");
+const Profile2 = require("./lib/managers.js");
+const Profile3 = require("./lib/engineers.js");
+const Profile4 = require("./lib/interns.js");
